05739a2d9242821b7b57081ecfc197a091dfa956
Woncheol Kim <icijiy@gmail.com>
2022-09-07 18:21:01 -0700

    Updated DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -20,8 +20,8 @@ export default function DayListItem(props) {
 
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
