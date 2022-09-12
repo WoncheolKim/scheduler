76846f4c80347c9847ec559fd7874d7f9c4947d9
Woncheol Kim <icijiy@gmail.com>
2022-09-12 16:13:32 -0700

    Building a Hooks

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,21 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
-} 
+  const transition = (newMode, replace = false) => {
+    if (!replace) {
+      setHistory((prev) => [...prev, newMode]);
+    }
+    setMode(newMode);
+  };
+
+  // -- transition back to previous mode
+  const back = () => {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
+  };
+  return { mode, transition, back };
+}
