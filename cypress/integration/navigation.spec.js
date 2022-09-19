476c2d9cef2a7d8f1f58bbebd39dfd7cb31dc2b2
Woncheol Kim <icijiy@gmail.com>
2022-09-18 22:54:35 -0700

    Testing book & edit & cancel

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,63 +1,34 @@
-describe("Navigation", () => {
-  it("should visit root", () => {
+describe("Appointments", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
     cy.visit("/");
-  });
-  it("Shoud navigate Tuesday", () => {
-    cy.contains('Tuesday') 
-      .click()
-  });
-  it("Should click the plus2 button", () => {
-    cy.get(':nth-child(2) > .appointment__add > .appointment__add-button')
-      .click()      
-  });
-  it("Shold click the cancel button", () => {
-    cy.get('.button--danger')
-      .click()
-  });
-  it("Shold click the cancel button", () => {
-    cy.get("li")
-    .contains("Tuesday")
-    .should("have.css", "background-color", "rgba(0, 0, 0, 0)");
-  });
-  it("Shold click the Monday button", () => {
-    cy.get("li")
-      .contains("Monday") 
-      .click()
+    cy.contains("Monday");
   });
 
   it("should book an interview", () => {
-    cy.get("[alt=Add]").first().click()
-    .get("[data-testid=student-name-input]").type("Lydia Miller-Jones")
-    .get('[alt="Sylvia Palmer"]').click()
-    .contains("Save").click()
-    .contains(".appointment__card--show", "Lydia Miller-Jones")
-    .contains(".appointment__card--show", "Sylvia Palmer")
-  });
-
-  it("should edit an interview", () => {
-    cy.get("[alt=Edit]").first().click({ force: true })
-    .get("[data-testid=student-name-input]")
-      .clear()
-      .type("Lydia Miller-Jones")
-    .get("[alt='Tori Malcolm']").click()
-    .contains("Save").click()
-    .contains(".appointment__card--show", "Lydia Miller-Jones")
-    .contains(".appointment__card--show", "Tori Malcolm")
+    cy.get("[alt=Add]").first().click();
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    cy.get('[alt="Sylvia Palmer"]').click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
   
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]").first().click({ force: true });
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+    cy.get('[alt="Tori Malcolm"]').click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+  });
+
   it("should cancel an interview", () => {
-    cy.get("[alt=Delete]").click({ force: true })
-    .contains("Confirm").click()
-    .contains("DELETE").should("exist")
-    .contains("DELETE").should("not.exist")
-    .contains(".appointment__card--show", "Archie Cohen").should("not.exist")
+    cy.get("[alt=Delete]").click({ force: true });
+    cy.contains("Confirm").click();
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
   });
-
-  it("Shold hover over the card", () => {
-    cy.get('[data-testid="student-name-input"]').clear();
-  });
-
-  it("Shold hover over the card", () => {
-    cy.get('[data-testid="student-name-input"]').type("abcd");
-  });
-}); 
+});
